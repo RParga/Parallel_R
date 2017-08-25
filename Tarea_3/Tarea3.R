d6b7c0a48ac7f2bba56fbd5bf7ddec3ae67535c4
jhernandez <jhernandez@ppgodel.mx>
2017-08-25 07:27:44 -0500

    added good result

--- Tarea_3/Tarea3.R
+++ Tarea_3/Tarea3.R
@@ -32,8 +32,8 @@ primax= function(n)
 
 
  
-desde <- 5
-hasta <-  500000
+desde <- 50000
+hasta <-  550000
 maxp = primax(hasta)
 
 original <- desde:hasta
@@ -42,33 +42,37 @@ pares = seq(2, 2*(hasta-desde),2)
 impares = seq(1, 2*(hasta-desde),2)
 maxprimo = rep(maxp,(hasta-desde))
 medium = c(rbind(rep(maxp,floor((hasta-desde)/2)), rep(2,(hasta-desde)/2 ) ))
-replicas <- 60
+replicas <- 40
 type = FALSE
 cores = detectCores() #- 1
 name= paste(desde,"_",hasta,"R",replicas,"C",cores, sep="" )
 
 #datos = data.frame( Tipo= character(),Nucleos= numeric(0), Time= double(0))
 datos = data.frame()
-for(core in 1:cores)
+for(core in 0:cores)
 {
+	print(core)
 	pt <-  numeric()
 	ot <-  numeric()
 	it <-  numeric()
 	at <-  numeric()
 	xt <-  numeric()
-	if(type)
-	{
-		registerDoParallel(makeCluster(cores))
-	}
-	else
-	{
-		cluster <- makeCluster(cores)
-		clusterExport(cluster, "primo")
+	if(core > 0)
+    {
+		if(type)
+		{
+			registerDoParallel(makeCluster(core))
+		}
+		else
+		{
+			cluster <- makeCluster(core)
+			clusterExport(cluster, "primo")
+		}
 	}
 	#print(paste('nucleos: ', core))
 	for (r in 1:replicas)
 	{
-	    if(core == 1)
+	    if(core == 0)
 	    {
 		    pt <- system.time(sapply(pares, primo))[3] # pares
 		    mt <- system.time(sapply(impares, primo))[3] # impares
@@ -83,22 +87,22 @@ for(core in 1:cores)
 
 		    if(type)
 		    {
-			pt <- system.time(foreach(n = pares, .combine=c) %dopar% primo(n))[3] # de menor a mayor
-			mt <- system.time(foreach(n = impares, .combine=c) %dopar% primo(n))[3] # de menor a mayor
-			ot <- system.time(foreach(n = original, .combine=c) %dopar% primo(n))[3] # de menor a mayor
+				pt <- system.time(foreach(n = pares, .combine=c) %dopar% primo(n))[3] # de menor a mayor
+				mt <- system.time(foreach(n = impares, .combine=c) %dopar% primo(n))[3] # de menor a mayor
+				ot <- system.time(foreach(n = original, .combine=c) %dopar% primo(n))[3] # de menor a mayor
 		    	it <- system.time(foreach(n = invertido, .combine=c) %dopar% primo(n))[3] # de mayor a menor
-			at <- system.time(foreach(n = sample(original), .combine=c) %dopar% primo(n))[3] # orden aleatorio
-			xt <- system.time(foreach(n = maxprimo, .combine=c) %dopar% primo(n))[3] # de menor a mayor
+				at <- system.time(foreach(n = sample(original), .combine=c) %dopar% primo(n))[3] # orden aleatorio
+				xt <- system.time(foreach(n = maxprimo, .combine=c) %dopar% primo(n))[3] # de menor a mayor
 		    	ht <- system.time(foreach(n = medium, .combine=c) %dopar% primo(n))[3] # mitad pares, mitad maxprimo
 		    }
 		    else
 		    {
-			pt = system.time(parSapply(cluster, pares, primo))[3]
-			mt = system.time(parSapply(cluster, impares, primo))[3]
-			ot = system.time(parSapply(cluster, original, primo))[3]
-			it = system.time(parSapply(cluster, invertido, primo))[3]
-			at = system.time(parSapply(cluster, sample(original), primo))[3]
-			xt = system.time(parSapply(cluster, maxprimo, primo))[3]
+				pt = system.time(parSapply(cluster, pares, primo))[3]
+				mt = system.time(parSapply(cluster, impares, primo))[3]
+				ot = system.time(parSapply(cluster, original, primo))[3]
+				it = system.time(parSapply(cluster, invertido, primo))[3]
+				at = system.time(parSapply(cluster, sample(original), primo))[3]
+				xt = system.time(parSapply(cluster, maxprimo, primo))[3]
 		    	ht = system.time(parSapply(cluster, medium, primo))[3]# mitad pares, mitad maxprimo
 		    }
 	    }
@@ -118,15 +122,19 @@ for(core in 1:cores)
 	    datos = rbind(datos,c('Medium', core, ht))
 	    print(paste('ot: ', ot, 'it: ', it,'at: ', at, 'pt: ', pt, 'mt: ', mt, 'xt: ', xt))
 	}
-	#if(type)
-	#{	
-	#}
-	#else
-	if(!type)
-	{
-		stopCluster(cluster)
-	}
 	
+	if(core > 0)
+    {
+		if(type)
+		{
+			stopImplicitCluster()	
+		}
+		else
+		if(!type)
+		{
+			stopCluster(cluster)
+		}
+	}
 }
 stopImplicitCluster()
 colnames(datos)= c('Tipo', 'Nucleos', 'Time')
