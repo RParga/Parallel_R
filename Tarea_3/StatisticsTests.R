caf8084f1b59dcbec00da4c4070b0897735308ef
jhernandez <jhernandez@ppgodel.mx>
2017-08-26 01:12:21 -0500

    added statistics

--- Tarea_3/StatisticsTests.R
+++ Tarea_3/StatisticsTests.R
@@ -1,11 +1,11 @@
-fname = ""
+fname = "datos50000_550000R40C8"
+equation = "datos$Time~Nucleos"
 alfa=0.0005
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)<0)
 {
     stop("At least two argument must be supplied (input file).\n", call.=FALSE)
-}
-else if (length(args)>0)
+} else if (length(args)>0)
 {
     fname = strtoi(args[1])
     equation = strtoi(args[2])
@@ -26,22 +26,38 @@ graphics.off()
 png(file=paste("qqresiduales_", fname,".png", sep=""))
 qqnorm(residuales)
 graphics.off()
-# prueba shapiro
-st = shaphiro.test(residuales)
-writelines(capture.output(print(ss)), file= paste("shapiro_",fname,".txt"))
+                                        # prueba shapiro
+fileConn<-file(paste("output", fname,".txt"))
+st = shapiro.test(residuales)
+writeLines(capture.output(print(st)))
 if(ss$p.value<alfa) # si el p.value es menor a alfa, los residuales **NO** siguen una distribucion normal
 {
-	#Pruebas no parametricas
-	kw<-kruskal.test(equation, data=datos)
-	writelines(capture.output(print(kw)), file= paste("kruskallWallis_",fname,".txt"))
+                                        #Pruebas no parametricas
+    print("Residuos NO normales, pruebas no parametricas")
+    kw<-kruskal.test(formula=equation, data=datos)
+    writelines(capture.output(print(kw)))
+    if(kw$p.value<alfa)
+    {
+                                        #Existe diferencia entre las varianzas, no son todas iguales, revisar cada factor para encontrar la diferencia
+        print("Existe diferencia entre las varianzas, no son todas iguales, revisar cada factor para encontrar la diferencia")
+        
+    } else{
+                                        #NO existe diferencia entre las varianzas, son todas iguales
+        print("NO existe diferencia entre las varianzas, son todas iguales")
+    }
+    
+	
 	
 }
 else
 {
-	#aplicar anova
-	anov=aov(linmod)
-	writelines(capture.output(print(anov)), file= paste("anova_",fname,".txt"))
+                                        #aplicar anova
+    print("Residuos normales, pruebas parametricas, ANOVA")
+    anov=aov(linmod)
+    writelines(capture.output(print(anov)), file= paste("anova_",fname,".txt"))
 	
 	
   
 }
+
+close(fileConn)
