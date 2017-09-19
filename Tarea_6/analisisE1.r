69e690f44905840fbb1468da3d7fa631e991b7c1
ppgodel <tashiro86@gmail.com>
2017-09-18 19:48:59 -0500

    added report

---
+++ Tarea_6/analisisE1.r
@@ -0,0 +1,6 @@
+datos = read.csv("datosepe1.csv")
+png("ComparacionDeProbVacInfect.png", width = 1200, height = 1200, units = "px", pointsize = 20)
+par(mar=c(6,6,4,2),cex.axis=1.5, cex.lab=1.8, cex.main=1.5)
+boxplot(datos$mi~datos$pv, ylim=c(0,100))
+#axis(side=1,at=c("1","2","3","4"), labels=c("Original","Original Paralelizado","Línea de barrido", "Línea de barrido paralelizdo"))
+#legend( title="Algoritmos",x="topright", inset=0,legend=c("Original","Original","Línea de barrido", "Línea de barrido paralelizdo"))
