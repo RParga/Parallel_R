1998a8bf98742e6e0ac91b7b95226eb3563541b8
ppgodel <tashiro86@gmail.com>
2017-10-16 13:46:21 -0500

    Added changes in practica4

--- Tarea_10/practica_4.r
+++ Tarea_10/practica_4.r
@@ -1,5 +1,7 @@
 library(testit)
- 
+
+replicas = 30
+reptime = data.frame(replicas=numeric(),time=numeric(),gap=numeric()) 
 knapsack <- function(cap, peso, valor) {
     n <- length(peso)
     pt <- sum(peso) 
@@ -82,57 +84,63 @@ reproduccion <- function(x, y, n) {
     yx <- c(y[1:pos], x[(pos+1):n])
     return(c(xy, yx))
 }
- 
-n <- 50
-pesos <- generador.pesos(n, 15, 80)
-valores <- generador.valores(pesos, 10, 500)
-capacidad <- round(sum(pesos) * 0.65)
-optimo <- knapsack(capacidad, pesos, valores)
-init <- 200
-p <- poblacion.inicial(n, init)
-tam <- dim(p)[1]
-assert(tam == init)
-pm <- 0.05
-rep <- 50
-tmax <- 50
-mejores <- double()
-tim = proc.time()[3]
-for (iter in 1:tmax) {
-    p$obj <- NULL
-    p$fact <- NULL
-    for (i in 1:tam) { # cada objeto puede mutarse con probabilidad pm
-        if (runif(1) < pm) {
-            p <- rbind(p, mutacion(p[i,], n))
-        }
-    }
-    for (i in 1:rep) { # una cantidad fija de reproducciones
-        padres <- sample(1:tam, 2, replace=FALSE)
-        hijos <- reproduccion(p[padres[1],], p[padres[2],], n)
-        p <- rbind(p, hijos[1:n]) # primer hijo
-        p <- rbind(p, hijos[(n+1):(2*n)]) # segundo hijo
-    }
-    tam <- dim(p)[1]
-    obj <- double()
-    fact <- integer()
-     for (i in 1:tam) {
-        obj <- c(obj, objetivo(p[i,], valores))
-        fact <- c(fact, factible(p[i,], pesos, capacidad))
-    }
-    p <- cbind(p, obj)
-    p <- cbind(p, fact)
-    mantener <- order(-p[, (n + 2)], -p[, (n + 1)])[1:init]
-    p <- p[mantener,]
+
+for(repli in 1:replicas){
+    n <- 50
+    pesos <- generador.pesos(n, 15, 80)
+    valores <- generador.valores(pesos, 10, 500)
+    capacidad <- round(sum(pesos) * 0.65)
+    optimo <- knapsack(capacidad, pesos, valores)
+    init <- 200
+    p <- poblacion.inicial(n, init)
     tam <- dim(p)[1]
     assert(tam == init)
-    factibles <- p[p$fact == TRUE,]
-    mejor <- max(factibles$obj)
-    mejores <- c(mejores, mejor)
+    pm <- 0.05
+    rep <- 50
+    tmax <- 50
+    mejores <- double()
+    tim = proc.time()[3]
+    for (iter in 1:tmax) {
+        p$obj <- NULL
+        p$fact <- NULL
+        for (i in 1:tam) { # cada objeto puede mutarse con probabilidad pm
+            if (runif(1) < pm) {
+                p <- rbind(p, mutacion(p[i,], n))
+            }
+        }
+        for (i in 1:rep) { # una cantidad fija de reproducciones
+            padres <- sample(1:tam, 2, replace=FALSE)
+            hijos <- reproduccion(p[padres[1],], p[padres[2],], n)
+            p <- rbind(p, hijos[1:n]) # primer hijo
+            p <- rbind(p, hijos[(n+1):(2*n)]) # segundo hijo
+        }
+        tam <- dim(p)[1]
+        obj <- double()
+        fact <- integer()
+        for (i in 1:tam) {
+            obj <- c(obj, objetivo(p[i,], valores))
+            fact <- c(fact, factible(p[i,], pesos, capacidad))
+        }
+        p <- cbind(p, obj)
+        p <- cbind(p, fact)
+        mantener <- order(-p[, (n + 2)], -p[, (n + 1)])[1:init]
+        p <- p[mantener,]
+        tam <- dim(p)[1]
+        assert(tam == init)
+        factibles <- p[p$fact == TRUE,]
+        mejor <- max(factibles$obj)
+        mejores <- c(mejores, mejor)
+    }
+    tim = proc.time()[3] - tim
+    print(tim)
+    png(paste("p10",repli,".png",sep=""), width=600, height=300)
+    plot(1:tmax, mejores, xlab="Paso", ylab="Mayor valor", type='l', ylim=c(0.95*min(mejores), 1.05*optimo))
+    points(1:tmax, mejores, pch=15)
+    abline(h=optimo, col="green", lwd=3)
+    graphics.off()
+    print(paste(mejor, (optimo - mejor) / optimo))
+    reptime = rbind(reptime,c(repli,tim, (optimo - mejor) / optimo))
 }
-tim = proc.time()[3] - tim
-print(tim)
-png("p10.png", width=600, height=300)
-plot(1:tmax, mejores, xlab="Paso", ylab="Mayor valor", type='l', ylim=c(0.95*min(mejores), 1.05*optimo))
-points(1:tmax, mejores, pch=15)
-abline(h=optimo, col="green", lwd=3)
-graphics.off()
-print(paste(mejor, (optimo - mejor) / optimo))
+colnames(reptime)= c("replica","time","gap")
+write.csv(reptime,"results.csv")
+print(reptime)
