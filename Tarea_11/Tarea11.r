5e55d01ef0764de0ad08f7f917169ed36d542298
ppgodel <tashiro86@gmail.com>
2017-10-23 21:35:01 -0500

    Added some results

--- Tarea_11/Tarea11.r
+++ Tarea_11/Tarea11.r
@@ -1,6 +1,6 @@
 library(ggplot2) # recordar instalar si hace falta
-library(Parallel)
-pick.one <- function(x) {
+library(parallel)
+pick.one <- function(x){
     if (length(x) == 1) {
         return(x)
     } else {
@@ -34,93 +34,129 @@ domin.by <- function(target, challenger, total) {
     } # si no empeora, vemos si hay mejora
     return(sum(challenger > target) > 0)
 }
-cluster = makeCluster(detectcores(logical=FALSE) )
+cluster = makeCluster(detectCores(logical=FALSE) )
 vc <- 4 #número de variables
 md <- 3 #máximo grado
 tc <- 5 #número de terminos
 #k <- 2 #cantidad de funciones objetivo
-replicas <- 30
-ns <- c(50 , 100, 200, 400, 800)
-ks = c(2,3,4,5,6)
-impr = FALSE
-paral=FALSE
+replicas <- 3
+ns <- 200#c(50 , 100, 200, 400, 800)
+ks = 2#c(2,3,4,5,6)
+impr = TRUE
+paral=TRUE
 results = data.frame(replicas=numeric(), ks=numeric(), ns=numeric(), time=numeric(), pnd=numeric())
 for(k in ks){
-    obj <- list()
-    for (i in 1:k) { # se crean las k funciones de manera aleatoria
-        obj[[i]] <- poli( md, vc, tc) 
-    }
-    minim <- (runif(k) > 0.5) #Se establece para cada k si se max o se min
-    sign <- (1 + -2 * minim) #Se establece el signo de cada función
-    n <- 200 # cuantas soluciones aleatorias
-    sol <- matrix(runif(vc * n), nrow=n, ncol=vc) #se crean valores aleatorios para cada variable de las n soluciones
-    val <- matrix(rep(NA, k * n), nrow=n, ncol=k) #se crean la matriz de valores de las evaluaciones de los elementos de cada solucion
-    for (i in 1:n) { # evaluamos las soluciones
-        for (j in 1:k) { # para todos los objetivos
-            val[i, j] <- eval(obj[[j]], sol[i,], tc)
-        }
-    }
-    if(impr){
+    for(rep in 1:replicas){
+        for(n in ns){
+            tim= proc.time()[3]
+            obj <- list()
+            for (i in 1:k) { # se crean las k funciones de manera aleatoria
+                obj[[i]] <- poli( md, vc, tc) 
+            }
+            minim <- (runif(k) > 0.5) #Se establece para cada k si se max o se min
+            sign <- (1 + -2 * minim) #Se establece el signo de cada función
+            #n <- 200 # cuantas soluciones aleatorias
+            sol <- matrix(runif(vc * n), nrow=n, ncol=vc) #se crean valores aleatorios para cada variable de las n soluciones
+            val <- matrix(rep(NA, k * n), nrow=n, ncol=k) #se crean la matriz de valores de las evaluaciones de los elementos de cada solucion
+            for (i in 1:n) { # evaluamos las soluciones
+                for (j in 1:k) { # para todos los objetivos
+                    val[i, j] <- eval(obj[[j]], sol[i,], tc)
+                }
+            }
+            if(impr){
                                         #cambiar para k elementos
-        mejor1 <- which.max(sign[1] * val[,1]) #se obtiene el mejor valor de funcion 1
-        mejor2 <- which.max(sign[2] * val[,2]) # se obtiene el mejor valor de funcion 2
-        cual <- c("max", "min")
-        xl <- paste("Primer objetivo (", cual[minim[1] + 1], ")", sep="")
-        yl <- paste("Segundo objetivo (", cual[minim[2] + 1], ")", sep="")
-        png("p11_init.png")
-        plot(val[,1], val[,2], xlab=xl, ylab=yl, main="Ejemplo bidimensional")
-        graphics.off()
-        png("p11_mejores.png")
-        plot(val[,1], val[,2], xlab=paste(xl, "mejor con cuadro azul"),
-             ylab=paste(yl,"mejor con bolita naranja"),
-             main="Ejemplo bidimensional")
-        points(val[mejor1, 1], val[mejor1, 2], col="blue", pch=15, cex=1.5)
-        points(val[mejor2, 1], val[mejor2, 2], col="orange", pch=16, cex=1.5)
-        graphics.off()
-    }
+                mejor1 <- which.max(sign[1] * val[,1]) #se obtiene el mejor valor de funcion 1
+                mejor2 <- which.max(sign[2] * val[,2]) # se obtiene el mejor valor de funcion 2
+                cual <- c("max", "min")
+                xl <- paste("Primer objetivo (", cual[minim[1] + 1], ")", sep="")
+                yl <- paste("Segundo objetivo (", cual[minim[2] + 1], ")", sep="")
+                png("p11_init.png")
+                plot(val[,1], val[,2], xlab=xl, ylab=yl, main="Ejemplo bidimensional")
+                graphics.off()
+                png("p11_mejores.png")
+                plot(val[,1], val[,2], xlab=paste(xl, "mejor con cuadro azul"),
+                     ylab=paste(yl,"mejor con bolita naranja"),
+                     main="Ejemplo bidimensional")
+                points(val[mejor1, 1], val[mejor1, 2], col="blue", pch=15, cex=1.5)
+                points(val[mejor2, 1], val[mejor2, 2], col="orange", pch=16, cex=1.5)
+                graphics.off()
+            }
                                         #frente de pareto
                                         #conjunto de los no dominados
-    no.dom <- logical()
-    dominadores <- integer()
-    for (i in 1:n) { #para casa solución
-        d <- logical() #vector de los dominados
-        if(paral){
-            clusterExport(cluster, "domin.by")
-            clusterExport(cluster, "sign")
-            clusterExport(cluster, "val")
-            clusterExport(cluster, "k")
-            clusterExport(cluster, "i")
-            d = parSapply(cluster,1:n, function(j){ return domin.by(sign * val[i,], sign * val[j,], k) })
-        }else{
+            no.dom <- logical()
+            dominadores <- integer()
             
-            for (j in 1:n) { #se compara contra TODAS las demas soluciones
-                d <- c(d, domin.by(sign * val[i,], sign * val[j,], k)) #se establece si para cada k hay una mejora o no 
+            
+            if(paral){
+                clusterExport(cluster, "domin.by")
+                clusterExport(cluster, "sign")
+                clusterExport(cluster, "val")
+                clusterExport(cluster, "k")
+                clusterExport(cluster, "i")
+                clusterExport(cluster, "n")
+                d = (parSapply(cluster, 1:n, function(j){
+                    for (j in 1:n) { #se compara contra TODAS las demas soluciones
+                        dp <- logical()
+                        for (j in 1:n) {
+                            dp <- c(dp, domin.by(sign * val[i,], sign * val[j,], k))
+                        }
+                        cuantos <- sum(dp)
+                        return(c(cuantos,cuantos==0))
+                    }
+                    return(cuantos)
+                }))                
+                dominadores = d[1,]
+                no.dom = as.logical(d[2,])
+                rm(d)
+            }else{
+                for (i in 1:n) { #para casa solución
+                d <- logical() #vector de los dominados
+                    for (j in 1:n) { #se compara contra TODAS las demas soluciones
+                        d <- c(d, domin.by(sign * val[i,], sign * val[j,], k)) #se establece si para cada k hay una mejora o no 
+                    }
+                }
             }
+            #print("CDF")
+            #print(d)
+            #cuantos <- (colSums(d))
+            #print(cuantos)
+            #dominadores <- c(dominadores, cuantos) #vector de los soluciones dominadas
+            #print(dominadores)
+            #no.dom <- c(no.dom, cuantos == 0) # se agrega si nadie le domina
+            #print(no.dom)
+            frente <- subset(val, no.dom) # solamente las no dominadas
+            print(frente)
+        
+            #if(impr){
+            #    png("p11_frente.png")
+            #    plot(val[,1], val[,2], xlab=paste(xl, "mejor con cuadro azul"),
+            #         ylab=paste(yl,"mejor con bolita naranja"),
+            #         main="Ejemplo bidimensional")
+            #    points(frente[,1], frente[,2], col="green", pch=16, cex=1.5)
+            #    graphics.off()
+            #    print("Frente")
+            #    print(dim(frente))
+            #    data <- data.frame(pos=rep(0, n), dom=dominadores)
+            #    print(data)
+            #    png(paste("p_", rep,"_11_hist.png",sep=""))
+            #    hist(data$dom)
+            #    graphics.off()
+            #    png(paste("p_", rep,"_11_violin.png",sep=""))
+            #    gr <- ggplot(data, aes(x=pos, y=dom)) + geom_violin(fill="orange", color="red")
+            #    gr + geom_boxplot(width=0.2, fill="blue", color="white", lwd=2) +
+            #    xlab("") +
+            #        ylab("Frecuencia") +
+            #    ggtitle("Cantidad de soluciones dominantes")
+            #    graphics.off()
+            #}
+            results = rbind(results, c(replica=rep, NE=k, NS=n, T=proc.time()[3]-tim, P=dim(frente)[1]*100/n ))
         }
-        cuantos <- sum(d)
-        dominadores <- c(dominadores, cuantos) #vector de los soluciones dominadas 
-        no.dom <- c(no.dom, cuantos == 0) # se agrega si nadie le domina
-    }
-    frente <- subset(val, no.dom) # solamente las no dominadas
-    
-    if(impr){
-        png("p11_frente.png")
-        plot(val[,1], val[,2], xlab=paste(xl, "mejor con cuadro azul"),
-             ylab=paste(yl,"mejor con bolita naranja"),
-             main="Ejemplo bidimensional")
-        points(frente[,1], frente[,2], col="green", pch=16, cex=1.5)
-        graphics.off()
-        data <- data.frame(pos=rep(0, n), dom=dominadores)
-        print(data)
-        png("p11_hist.png")
-        hist(data$dom)
-        graphics.off()
-        png("p11_violin.png")
-        gr <- ggplot(data, aes(x=pos, y=dom)) + geom_violin(fill="orange", color="red")
-        gr + geom_boxplot(width=0.2, fill="blue", color="white", lwd=2) +
-            xlab("") +
-            ylab("Frecuencia") +
-            ggtitle("Cantidad de soluciones dominantes")
-        graphics.off()
     }
 }
+tipo="Secuencial"
+if(paral){
+    tipo="Parallelo"
+}
+colnames(results)=c("replica", "NE", "NS", "T", "P")
+print(results)
+write.csv(results, paste(tipo,"_data.csv",sep=""))
