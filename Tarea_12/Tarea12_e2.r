91cee616c14e1669b4e7850d78d7764190e3ff53
ppgodel <tashiro86@gmail.com>
2017-10-29 22:53:18 -0600

    Added final version of experiments

--- Tarea_12/Tarea12_e2.r
+++ Tarea_12/Tarea12_e2.r
@@ -31,95 +31,101 @@ if(pll){
 r <- 5
 c <- 3
 dim <- r * c
-train=5000
+trains=c(5000,10000,15000)
+#train=5000
 tests= c(600)
 tasa <- 0.15
 tranqui <- 0.99
 library('parallel')
 cluster = makeCluster(detectCores(logical=FALSE))
-tope <- 21
-digitos <- 0:tope
-k <- length(digitos)
-n <- floor(log(k-1, 2)) + 1
-result = data.frame(R=numeric(),M=numeric(), T=numeric(), P=numeric())
+topes=c(9,12,15,18,21)
+#tope <- 21
+result = data.frame(R=numeric(),M=numeric(),E=numeric(), T=numeric(), P=numeric())
 
 for(test in tests){
-    for(rep in 1:replicas){
-        tasa <- 0.15
-        neuronas <- matrix(runif(n * dim), nrow=n, ncol=dim) # perceptrones, uno por cada posicion de la presentacion binaria log(digito,2)
-        tim=proc.time()[3]
-        for (t in 1:train) { # entrenamiento
-            d <- sample(0:tope, 1) # escoge una posicion al azar entre el número y el tope
-            pixeles <- runif(dim) < modelos[d + 1,] #crea una imagen aleatoria de la posicion deacuerdo al modelo que le corresponde
-            correcto <- binario(d, n) #obtiene el resultado "real" en representacion binaria 
-            for (i in 1:n) { #para cada neurona, la cual "percibe" cada posicion binaria
-                w <- neuronas[i,] # obtiene su vector de pesos
-                deseada <- correcto[i] # obtiene el valor correspondiente a la posicion si es true o false para el numero selccionado d
-                resultado <- sum(w * pixeles) >= 0 # si el pixel multiplicado por el peso es mayor o igual a cero asigna True en caso contrario false
-                #print(paste(deseada,resultado, d))
-                if (deseada != resultado) { # si la suma de pixeles y los pesos no es igual al valor deseado
-                    ajuste <- tasa * (deseada - resultado) # calcula el ajuste necesario
-                    tasa <- tranqui * tasa #disminuye la tasa de cambio para futuros cambios
-                    neuronas[i,] <- w + ajuste * pixeles #realiza el ajuste
+    for(tope in topes){
+        for(train in trains){
+            digitos <- 0:tope
+            k <- length(digitos)
+            n <- floor(log(k-1, 2)) + 1
+            for(rep in 1:replicas){
+                tasa <- 0.15
+                neuronas <- matrix(runif(n * dim), nrow=n, ncol=dim) # perceptrones, uno por cada posicion de la presentacion binaria log(digito,2)
+                tim=proc.time()[3]
+                for (t in 1:train) { # entrenamiento
+                    d <- sample(0:tope, 1) # escoge una posicion al azar entre el número y el tope
+                    pixeles <- runif(dim) < modelos[d + 1,] #crea una imagen aleatoria de la posicion deacuerdo al modelo que le corresponde
+                    correcto <- binario(d, n) #obtiene el resultado "real" en representacion binaria 
+                    for (i in 1:n) { #para cada neurona, la cual "percibe" cada posicion binaria
+                        w <- neuronas[i,] # obtiene su vector de pesos
+                        deseada <- correcto[i] # obtiene el valor correspondiente a la posicion si es true o false para el numero selccionado d
+                        resultado <- sum(w * pixeles) >= 0 # si el pixel multiplicado por el peso es mayor o igual a cero asigna True en caso contrario false
+                                        #print(paste(deseada,resultado, d))
+                        if (deseada != resultado) { # si la suma de pixeles y los pesos no es igual al valor deseado
+                            ajuste <- tasa * (deseada - resultado) # calcula el ajuste necesario
+                            tasa <- tranqui * tasa #disminuye la tasa de cambio para futuros cambios
+                            neuronas[i,] <- w + ajuste * pixeles #realiza el ajuste
+                        }
+                    }
                 }
+                
+                
+                if(pll){
+                    clusterExport(cluster, "tope")
+                    clusterExport(cluster, "binario")
+                    clusterExport(cluster, "modelos")
+                    clusterExport(cluster, "dim")
+                    clusterExport(cluster, "neuronas")
+                    clusterExport(cluster, "n")
+                    clusterExport(cluster, "k")
+                    clusterExport(cluster, "decimal")
+                    
+                    res = parSapply(cluster, 1:test, function(x){
+                        d <- sample(0:tope, 1)
+                        pixeles <- runif(dim) < modelos[d + 1,] # fila 1 contiene el cero, etc.
+                        correcto <- binario(d, n)
+                        salida <- rep(FALSE, n)
+                        for (i in 1:n) {
+                            w <- neuronas[i,]
+                            deseada <- correcto[i]
+                            resultado <- sum(w * pixeles) >= 0
+                            salida[i] <- resultado
+                        }
+                        r <- min(decimal(salida, n), k) # todos los no-existentes van al final
+                                        #contadores[d+1, r+1] <- contadores[d+1, r+1] + 1
+                        return(t(c(d,r)))
+                    })
+                    resm = matrix(res,ncol=2, byrow=TRUE)
+                }else{
+                    contadores <- matrix(rep(0, k*(k+1)), nrow=k, ncol=(k+1))
+                    rownames(contadores) <- 0:tope
+                    colnames(contadores) <- c(0:tope, NA)
+                    resm <- matrix(rep(0,2*test),ncol=2) #matrix(res,ncol=2, byrow=TRUE)
+                    for (t in 1:test) { # prueba
+                        d <- sample(0:tope, 1)
+                        pixeles <- runif(dim) < modelos[d + 1,] # fila 1 contiene el cero, etc.
+                        correcto <- binario(d, n)
+                        salida <- rep(FALSE, n)
+                        for (i in 1:n) {
+                            w <- neuronas[i,]
+                            deseada <- correcto[i]
+                            resultado <- sum(w * pixeles) >= 0
+                            salida[i] <- resultado
+                        }
+                        r <- min(decimal(salida, n), k) # todos los no-existentes van al final
+                                        #contadores[d+1, r+1] <- contadores[d+1, r+1] + 1
+                        resm[t,1] <- d
+                        resm[t,2] <- r
+                    }
+                }
+                tim=proc.time()[3]-tim
+                #print(resm)
+                porc=sum(resm[,1]-resm[,2] == 0)/test*100
+                pr = c(R=rep,M=tope,E=train,T=tim, P=porc)
+                print(pr)
+                result = rbind(result,pr)
             }
         }
-        
-        
-        if(pll){
-            clusterExport(cluster, "tope")
-            clusterExport(cluster, "binario")
-            clusterExport(cluster, "modelos")
-            clusterExport(cluster, "dim")
-            clusterExport(cluster, "neuronas")
-            clusterExport(cluster, "n")
-            clusterExport(cluster, "k")
-            clusterExport(cluster, "decimal")
-            
-            res = parSapply(cluster, 1:test, function(x){
-                d <- sample(0:tope, 1)
-                pixeles <- runif(dim) < modelos[d + 1,] # fila 1 contiene el cero, etc.
-                correcto <- binario(d, n)
-                salida <- rep(FALSE, n)
-                for (i in 1:n) {
-                    w <- neuronas[i,]
-                    deseada <- correcto[i]
-                    resultado <- sum(w * pixeles) >= 0
-                    salida[i] <- resultado
-                }
-                r <- min(decimal(salida, n), k) # todos los no-existentes van al final
-                                        #contadores[d+1, r+1] <- contadores[d+1, r+1] + 1
-                return(t(c(d,r)))
-            })
-            resm = matrix(res,ncol=2, byrow=TRUE)
-        }else{
-            contadores <- matrix(rep(0, k*(k+1)), nrow=k, ncol=(k+1))
-            rownames(contadores) <- 0:tope
-            colnames(contadores) <- c(0:tope, NA)
-            resm <- matrix(rep(0,2*test),ncol=2) #matrix(res,ncol=2, byrow=TRUE)
-            for (t in 1:test) { # prueba
-                d <- sample(0:tope, 1)
-                pixeles <- runif(dim) < modelos[d + 1,] # fila 1 contiene el cero, etc.
-                correcto <- binario(d, n)
-                salida <- rep(FALSE, n)
-                for (i in 1:n) {
-                    w <- neuronas[i,]
-                    deseada <- correcto[i]
-                    resultado <- sum(w * pixeles) >= 0
-                    salida[i] <- resultado
-                }
-                r <- min(decimal(salida, n), k) # todos los no-existentes van al final
-                                        #contadores[d+1, r+1] <- contadores[d+1, r+1] + 1
-                resm[t,1] <- d
-                resm[t,2] <- r
-            }
-        }
-        tim=proc.time()[3]-tim
-        print(resm)
-        porc=sum(resm[,1]-resm[,2] == 0)/test*100
-        pr = c(R=rep,M=test,T=tim, P=porc)
-        print(pr)
-        result = rbind(result,pr)
-    }
+    }    
 }
 write.csv(result, paste("datos_",tipo,"_T12_E2.csv",sep=""))
--- Tarea_12/analisis.r
+++ Tarea_12/analisis.r
@@ -9,9 +9,37 @@ colnames(datas)=c('Y','R','M','T','P')
 results = rbind(datap,datas)
 
 png(paste("bxplt_Tiempo_TipoM.png",sep=""),width=960, height=480)
-boxplot(results$T ~ results$Y+as.factor(results$M), xlab='Tipo', ylab="Tiempo")
+boxplot(results$T ~ results$Y+as.factor(results$M), xlab='Tipo', ylab="Tiempo", las=2, names=c('P-300','S-300','P-600','S-600','P-1200','S-1200'))
 graphics.off()
 png(paste("bxplt_P_TipoM.png",sep=""),width=960, height=480)
-boxplot(results$P ~ results$Y+as.factor(results$M), xlab='Tipo', ylab="Porcentaje de separación")
+boxplot(results$P ~ results$Y+as.factor(results$M), xlab='Tipo', ylab="Porcentaje de aciertos", las=2, names=c('P-300','S-300','P-600','S-600','P-1200','S-1200'))
 graphics.off()
 
+
+datae1=read.csv("datos_Paralelo_T12_E1.csv", stringsAsFactors=F)
+datae1$X=NULL
+colnames(datae1)=c('R','M','PN','PG','PB','T','P')
+result = datae1[datae1$PN==0,]
+png(paste("bxplt_P_PSN.png",sep=""),width=960, height=480)
+boxplot(P ~ as.factor(PN)+as.factor(PB)+as.factor(PG), las=2, data=result, names=c('NNN','NNB','NNM','NNA','NBN','NBB','NBM','NBA','NMN','NMB','NMM','NMA','NAN','NAB','NAM','NAA'), xlab='Tipo', ylab="Porcentaje")
+graphics.off()
+result = datae1[which(round(datae1$PN,digits=3)==0.333),]
+png(paste("bxplt_P_PSB.png",sep=""),width=960, height=480)
+boxplot(P ~ as.factor(PN)+as.factor(PB)+as.factor(PG), las=2, data=result, names=c('BNN','BNB','BNM','BNA','BBN','BBB','BBM','BBA','BMN','BMB','BMM','BMA','BAN','BAB','BAM','BAA'), xlab='Tipo', ylab="Porcentaje")
+graphics.off()
+result = datae1[which(round(datae1$PN,digits=3)==0.667),]
+png(paste("bxplt_P_PSM.png",sep=""),width=960, height=480)
+boxplot(P ~ as.factor(PN)+as.factor(PB)+as.factor(PG), las=2, data=result, names=c('MNN','MNB','MNM','MNA','MBN','MBB','MBM','MBA','MMN','MMB','MMM','MMA','MAN','MAB','MAM','MAA'), xlab='Tipo', ylab="Porcentaje")
+graphics.off()
+result = datae1[datae1$PN==1,]
+png(paste("bxplt_P_PSA.png",sep=""),width=960, height=480)
+boxplot(P ~ as.factor(PN)+as.factor(PB)+as.factor(PG), las=2, data=result, names=c('ANN','ANB','ANM','ANA','ABN','ABB','ABM','ABA','AMN','AMB','AMM','AMA','AAN','AAB','AAM','AAA'), xlab='Tipo', ylab="Porcentaje")
+graphics.off()
+
+
+datae2=read.csv("datos_Paralelo_T12_E2.csv", stringsAsFactors=F)
+datae2$X=NULL
+colnames(datae2)=c('R','M','E','T','P')
+png(paste("bxplt_P_CHE5000.png",sep=""),width=960, height=480)
+boxplot(datae2$P~datae2$M+datae2$E, xlab='Cantidad de Caracteres', ylab='Porcentaje de aciertos',las=2)
+graphics.off()
